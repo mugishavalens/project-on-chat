f93220657a3fcac1ff33026f6123d823b480f74d
unknown <migishavalens>
2025-04-11 17:00:53 +0200

    working on font-family

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-const text = document.querySelector("input");
+const text = document.querySelector("textarea");
 const send = document.querySelector("button");
 const display = document.querySelector(".output");
 
@@ -6,6 +6,6 @@ send.addEventListener("click", function(){
     let newmssg = document.createElement("p");
     newmssg.textContent = text.value;
     display.appendChild(newmssg);
-    newmssg.style = "max-width:120px; margin-left:50px; border-radius:20px; max-height:100px; background-color:white; display:flex; align-items:center; justify-content:flex-start; padding:10px; flex-direction: column; overflow-y: auto; margin-top:10px;color: hsl(271, 15%, 43%)"
+    newmssg.style = "max-width:120px;max-height:100px;margin-left:50px; border-radius:10px 10px 0% 10px; background-color:white; display:flex; font-size:11px; align-items:center; justify-content:center; flex-direction: column;color: hsl(271, 15%, 43%); margin-bottom:10px; padding:10px"
     text.value = ""
 })
